4d6e81a58f061ccd4340bad7615288985c89d0c1
Gustavo <gustavo_bortolon@hotmail.com>
2022-02-24 11:17:05 -0300

    Adicionar arquivos de projeto.

---
+++ cotacaodolar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace cotacaodolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Qual a cotacao do dolar:");
+            double dolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Quantos dolares voce vai comprar:");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double result = Cotacao.DolarParaReal(dolar, quantidade);
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
